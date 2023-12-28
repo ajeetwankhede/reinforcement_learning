4cf05e9215c7f292addeff68488f8d8d48574e9b
ajeetwankhede <ajeet@umd.edu>
2023-12-27 16:03:51 -0800

    feat(cart_pole): add q-learning scripts

---
+++ cart_pole/cart_pole_dqn.py
@@ -0,0 +1,242 @@
+# https://blog.gofynd.com/building-a-deep-q-network-in-pytorch-fa1086aa5435
+
+import torch
+import torch.nn as nn
+from torch.distributions.normal import Normal
+
+from pathlib import Path
+from typing import NamedTuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from tqdm import tqdm
+
+from collections import deque
+import copy
+from random import *
+
+import gymnasium as gym
+
+sns.set_theme()
+
+class Params(NamedTuple):
+    total_episodes: int
+    layer_sizes: int
+    sync_freq: int
+    exp_replay_size: int
+    batch_size: int
+    learning_rate: float
+    gamma: float
+    seed: int
+    action_size: int
+    state_size: int
+    savefig_folder: Path
+    model_path: Path
+
+params = Params(
+    total_episodes=7000,
+    layer_sizes=[],
+    sync_freq = 5,
+    exp_replay_size = 256,
+    batch_size = 16,
+    learning_rate=1e-3,
+    gamma=0.95,
+    seed=1423,
+    action_size=2,
+    state_size=4,
+    savefig_folder=Path("cart_pole/"),
+    model_path=Path('cart_pole/cart_pole_model'),
+)
+params = params._replace(layer_sizes=[params.state_size, 64, params.action_size])
+print(params)
+# Create the figure folder if it doesn't exists
+params.savefig_folder.mkdir(parents=True, exist_ok=True)
+
+class DQNlearning:
+    def __init__(self, seed, layer_sizes, learning_rate, gamma, state_size, action_size, sync_freq, exp_replay_size, batch_size):
+        torch.manual_seed(seed)
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.learning_rate = learning_rate
+        self.gamma = torch.tensor(gamma).float()
+        self.exp_replay_size = exp_replay_size
+        self.experience_replay = deque(maxlen=exp_replay_size)
+        self.batch_size = batch_size
+        
+        self.network_sync_freq = sync_freq
+        self.network_sync_counter = 0
+        self.net = self.build_nn(layer_sizes)
+        self.target_net = self.build_nn(layer_sizes)
+
+        self.loss_fn = torch.nn.MSELoss()
+        self.optimizer = torch.optim.AdamW(self.net.parameters(), lr=self.learning_rate)
+        
+    def build_nn(self, layer_sizes):
+        assert len(layer_sizes) > 1
+        layers = []
+        for index in range(len(layer_sizes)-1):
+            linear = nn.Linear(layer_sizes[index], layer_sizes[index+1])
+            act =    nn.Tanh() if index < len(layer_sizes)-2 else nn.Identity()
+            layers += (linear,act)
+        return nn.Sequential(*layers)
+    
+    def get_q_next(self, state):
+        with torch.no_grad():
+            Qp = self.target_net(torch.tensor(state))
+        
+        Q, _ = torch.max(Qp, axis=1)
+        return Q
+    
+    def sample_from_experience(self):
+        sample_exp = sample(self.experience_replay, self.batch_size)
+        state = torch.tensor([exp[0] for exp in sample_exp]).float()
+        action = torch.tensor([exp[1] for exp in sample_exp]).float()
+        reward = torch.tensor([exp[2] for exp in sample_exp]).float()
+        new_state = torch.tensor([exp[3] for exp in sample_exp]).float()
+        return state, action, reward, new_state
+
+    def train(self):
+        """Update Q(s,a):= Q(s,a) + alpha [R(s,a) + gamma * max Q(s',a') - Q(s,a)]"""
+        state, action, reward, new_state = self.sample_from_experience()
+
+        if self.network_sync_counter == self.network_sync_freq:
+            self.target_net.load_state_dict(self.net.state_dict())
+            self.network_sync_counter = 0
+
+        # predicted Q
+        Qp = self.net(torch.tensor(state))
+        q_pre, _ = torch.max(Qp, axis=1)
+
+        # expected Q
+        q_next = self.get_q_next(new_state)
+        q_exp = reward + self.gamma * q_next
+
+        loss = self.loss_fn(q_pre, q_exp)
+        self.optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        self.optimizer.step()
+
+        self.network_sync_counter += 1
+        return loss.item()
+
+    def choose_action(self, state, epsilon):
+        explor_exploit_tradeoff = torch.rand(1,).item()
+
+        if explor_exploit_tradeoff < epsilon:
+            action = torch.randint(0,self.action_size,(1,))
+        else:
+            with torch.no_grad():
+                Qp = self.net(torch.from_numpy(state).float())
+            Q, action = torch.max(Qp, axis=0)
+        return action.item()
+
+def run_env():
+    rewards = np.zeros((params.total_episodes, 1))
+    steps = np.zeros((params.total_episodes, 1))
+    epsilons = np.zeros((params.total_episodes, 1))
+    lossess = np.zeros((params.total_episodes, 1))
+    episodes = np.arange(params.total_episodes)
+
+    index = 0
+    epsilon = 1
+    for episode in tqdm(
+        episodes, desc=f"Run Episodes", leave=False
+    ):
+        state = env.reset()[0]
+        step = 0
+        done = False
+        total_rewards = 0
+        total_losses = 0
+
+        while not done:
+            action = agent.choose_action(
+                state=state,
+                epsilon=epsilon,
+            )
+
+            new_state, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            agent.experience_replay.append([state, action, reward, new_state])
+
+            if index > 128:
+                index = 0
+                for j in range(4):
+                    loss = agent.train()
+                    total_losses += loss
+
+            state = new_state
+            total_rewards += reward
+            step += 1
+            index +=1
+
+        if epsilon > 0.05:
+            epsilon -= (1/5000)
+        
+        rewards[episode] = total_rewards
+        epsilons[episode] = epsilon
+        lossess[episode] = total_losses/step
+        
+    return rewards, epsilons, lossess
+
+def run_model():
+    state = env.reset()[0]
+    done = False
+    total_rewards = 0
+    epsilon = 0.0
+    while not done:
+        action = agent.choose_action(
+            state=state,
+            epsilon=epsilon,
+        )
+        
+        new_state, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+
+        state = new_state
+        total_rewards += reward
+        
+    return total_rewards
+
+agent = DQNlearning(
+    seed=params.seed,
+    layer_sizes=params.layer_sizes,
+    learning_rate=params.learning_rate,
+    gamma=params.gamma,
+    state_size=params.state_size,
+    action_size=params.action_size,
+    sync_freq=params.sync_freq,
+    exp_replay_size=params.exp_replay_size,
+    batch_size=params.batch_size,
+)
+
+mode = input("Train or Replay (T/R)?: ")
+if (mode == "T"):
+    env = gym.make(
+    "CartPole-v1")
+    rewards, epsilons, lossess = run_env()
+    print(rewards[-10:])
+    save = input("Save model (Y/N)?: ")
+    if save == "Y":
+        torch.save(agent.net.state_dict(), params.model_path)
+        fig, ax = plt.subplots(nrows=3, ncols=1)
+        ax[0].plot(rewards)
+        ax[0].set_title("rewards")
+        ax[1].plot(epsilons)
+        ax[1].set_title("epsilons")
+        ax[2].plot(lossess)
+        ax[2].set_title("lossess")
+        fig.savefig(params.savefig_folder, bbox_inches="tight")
+        plt.show()
+    
+else:
+    env = gym.make(
+    "CartPole-v1", 
+    render_mode='human')
+    agent.net.load_state_dict(torch.load(str(params.model_path)+"_dqn_baseline"))
+    rewards = run_model()
+    print(rewards)
+
+env.close()
---
+++ frozen_lake/frozen_lake_q_learning.py
@@ -0,0 +1,254 @@
+from pathlib import Path
+from typing import NamedTuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from tqdm import tqdm
+
+import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+
+sns.set_theme()
+
+class Params(NamedTuple):
+    total_episodes: int
+    learning_rate: float
+    gamma: float
+    epsilon: float
+    map_size: int
+    seed: int
+    is_slippery: bool
+    n_runs: int
+    action_size: int
+    state_size: int
+    proba_frozen: float
+    savefig_folder: Path
+
+params = Params(
+    total_episodes=200,
+    learning_rate=0.8,
+    gamma=0.95,
+    epsilon=0.1,
+    map_size=5,
+    seed=123,
+    is_slippery=False,
+    n_runs=1,
+    action_size=None,
+    state_size=None,
+    proba_frozen=0.9,
+    savefig_folder=Path("frozen_lake/")
+)
+print(params)
+
+# Set the seed
+rng = np.random.default_rng(params.seed)
+
+# Create the figure folder if it doesn't exists
+params.savefig_folder.mkdir(parents=True, exist_ok=True)
+
+class Qlearning:
+    def __init__(self, learning_rate, gamma, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.reset_qtable()
+
+    def update(self, state, action, reward, new_state):
+        """Update Q(s,a):= Q(s,a) + alpha [R(s,a) + gamma * max Q(s',a') - Q(s,a)]"""
+        delta = (
+            reward
+            + self.gamma * np.max(self.qtable[new_state, :])
+            - self.qtable[state, action]
+        )
+        q_update = self.qtable[state, action] + self.learning_rate * delta
+        return q_update
+
+    def reset_qtable(self):
+        self.qtable = np.zeros((self.state_size, self.action_size))
+
+class EpsilonGreedy:
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+
+    def choose_action(self, action_space, state, qtable):
+        explor_exploit_tradeoff = rng.uniform(0, 1)
+
+        if explor_exploit_tradeoff < self.epsilon:
+            action = action_space.sample()
+        else:
+            if np.all(qtable[state, :] == qtable[state, 0]):
+                action = action_space.sample()
+            else:
+                action = np.argmax(qtable[state, :])
+        
+        return action
+
+def run_env():
+    rewards = np.zeros((params.total_episodes, params.n_runs))
+    steps = np.zeros((params.total_episodes, params.n_runs))
+    episodes = np.arange(params.total_episodes)
+    qtables = np.zeros((params.n_runs, params.state_size, params.action_size))
+    all_states = []
+    all_actions = []
+
+    for run in range(params.n_runs):
+        learner.reset_qtable()
+        for episode in tqdm(
+            episodes, desc=f"Run {run}/{params.n_runs} - Episodes", leave=False
+        ):
+          state = env.reset(seed=params.seed)[0]
+          step = 0
+          done = False
+          total_rewards = 0
+
+          while not done:
+              action = explorer.choose_action(
+                  action_space=env.action_space, state=state, qtable=learner.qtable
+              )
+              all_states.append(state)
+              all_actions.append(action)
+
+              new_state, reward, terminated, truncated, info = env.step(action)
+
+              done = terminated or truncated
+
+              learner.qtable[state, action] = learner.update(
+                  state, action, reward, new_state
+              )
+
+              total_rewards += reward
+              step += 1
+
+              state = new_state
+            
+          rewards[episode, run] = total_rewards
+          steps[episode, run] = step
+        
+        qtables[run, :, :] = learner.qtable
+
+    return rewards, steps, episodes, qtables, all_states, all_actions
+          
+def postprocess(episodes, params, rewards, steps, map_size):
+    """Convert the results of the simulation in dataframes."""
+    res = pd.DataFrame(
+        data={
+            "Episodes": np.tile(episodes, reps=params.n_runs),
+            "Rewards": rewards.flatten(),
+            "Steps": steps.flatten(),
+        }
+    )
+    res["cum_rewards"] = rewards.cumsum(axis=0).flatten(order="F")
+    res["map_size"] = np.repeat(f"{map_size}x{map_size}", res.shape[0])
+
+    st = pd.DataFrame(data={"Episodes": episodes, "Steps": steps.mean(axis=1)})
+    st["map_size"] = np.repeat(f"{map_size}x{map_size}", st.shape[0])
+    return res, st
+
+def qtable_directions_map(qtable, map_size):
+    """Get the best learned action & map it to arrows."""
+    qtable_val_max = qtable.max(axis=1).reshape(map_size, map_size)
+    qtable_best_action = np.argmax(qtable, axis=1).reshape(map_size, map_size)
+    directions = {0: "←", 1: "↓", 2: "→", 3: "↑"}
+    qtable_directions = np.empty(qtable_best_action.flatten().shape, dtype=str)
+    eps = np.finfo(float).eps  # Minimum float number on the machine
+    for idx, val in enumerate(qtable_best_action.flatten()):
+        if qtable_val_max.flatten()[idx] > eps:
+            # Assign an arrow only if a minimal Q-value has been learned as best action
+            # otherwise since 0 is a direction, it also gets mapped on the tiles where
+            # it didn't actually learn anything
+            qtable_directions[idx] = directions[val]
+    qtable_directions = qtable_directions.reshape(map_size, map_size)
+    return qtable_val_max, qtable_directions
+
+def plot_q_values_map(qtable, env, map_size):
+    """Plot the last frame of the simulation and the policy learned."""
+    qtable_val_max, qtable_directions = qtable_directions_map(qtable, map_size)
+
+    # Plot the last frame
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+    ax[0].imshow(env.render())
+    ax[0].axis("off")
+    ax[0].set_title("Last frame")
+
+    # Plot the policy
+    sns.heatmap(
+        qtable_val_max,
+        annot=qtable_directions,
+        fmt="",
+        ax=ax[1],
+        cmap=sns.color_palette("Blues", as_cmap=True),
+        linewidths=0.7,
+        linecolor="black",
+        xticklabels=[],
+        yticklabels=[],
+        annot_kws={"fontsize": "xx-large"},
+    ).set(title="Learned Q-values\nArrows represent best action")
+    for _, spine in ax[1].spines.items():
+        spine.set_visible(True)
+        spine.set_linewidth(0.7)
+        spine.set_color("black")
+    img_title = f"frozenlake_q_values_{map_size}x{map_size}.png"
+    fig.savefig(params.savefig_folder / img_title, bbox_inches="tight")
+    plt.show()
+
+def plot_states_actions_distribution(states, actions, map_size):
+    """Plot the distributions of states and actions."""
+    labels = {"LEFT": 0, "DOWN": 1, "RIGHT": 2, "UP": 3}
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+    sns.histplot(data=states, ax=ax[0], kde=True)
+    ax[0].set_title("States")
+    sns.histplot(data=actions, ax=ax[1])
+    ax[1].set_xticks(list(labels.values()), labels=labels.keys())
+    ax[1].set_title("Actions")
+    fig.tight_layout()
+    img_title = f"frozenlake_states_actions_distrib_{map_size}x{map_size}.png"
+    fig.savefig(params.savefig_folder / img_title, bbox_inches="tight")
+    plt.show()
+
+# map_sizes = [4, 7, 9, 11]
+map_sizes = [7]
+res_all = pd.DataFrame()
+st_all = pd.DataFrame()
+
+for map_size in map_sizes:
+    env = gym.make(
+        "FrozenLake-v1",
+        is_slippery=params.is_slippery,
+        render_mode="rgb_array",
+        desc=generate_random_map(
+            size=map_size, p=params.proba_frozen, seed=params.seed
+        ),
+    )
+    params = params._replace(action_size=env.action_space.n)
+    params = params._replace(state_size=env.observation_space.n)
+    env.action_space.seed(
+        params.seed
+    )
+    learner = Qlearning(
+        learning_rate=params.learning_rate,
+        gamma=params.gamma,
+        state_size=params.state_size,
+        action_size=params.action_size,
+    )
+    explorer = EpsilonGreedy(
+        epsilon=params.epsilon,
+    )
+    print(f"Map size: {map_size}x{map_size}")
+    rewards, steps, episodes, qtables, all_states, all_actions = run_env()
+
+    # Save the results in dataframes
+    res, st = postprocess(episodes, params, rewards, steps, map_size)
+    res_all = pd.concat([res_all, res])
+    st_all = pd.concat([st_all, st])
+    qtable = qtables.mean(axis=0)  # Average the Q-table between runs
+
+    plot_states_actions_distribution(
+        states=all_states, actions=all_actions, map_size=map_size
+    )  # Sanity check
+    plot_q_values_map(qtable, env, map_size)
+
+    env.close()
