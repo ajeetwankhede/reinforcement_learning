4014c9f3eb2917f4e266e3854805e311ba707f32
ajeetwankhede <ajeet@umd.edu>
2024-03-25 09:03:28 -0700

    test(lunar_lander): commit the saved weights

--- lunar_lander/lunar_lander_a2c.py
+++ lunar_lander/lunar_lander_a2c.py
@@ -236,143 +236,144 @@ critic_losses = []
 actor_losses = []
 entropies = []
 
-# use tqdm to get a progress bar for training
-for sample_phase in tqdm(range(n_updates)):
-    # we don't have to reset the envs, they just continue playing
-    # until the episode is over and then reset automatically
-    
-    # reset lists that collect experiences of an episode (sample phase)
-    ep_value_preds = torch.zeros(n_steps_per_update, n_envs, device=device)
-    ep_rewards = torch.zeros(n_steps_per_update, n_envs, device=device)
-    ep_action_log_probs = torch.zeros(n_steps_per_update, n_envs, device=device)
-    masks = torch.zeros(n_steps_per_update, n_envs, device=device)
-
-    # at the start of training reset all envs to get an initial state
-    if sample_phase == 0:
-        states, info = envs_wrapper.reset(seed=42)
-
-    # play n steps in our parallel environments to collect data
-    for step in range(n_steps_per_update):
-        # select an action A_{t} using S_{t} as input for the agent
-        actions, action_log_probs, state_value_preds, entropy = agent.select_action(
-            states
-        )
-
-        # perform the action A_{t} in the environment to get S_{t+1} and R_{t+1}
-        states, rewards, terminated, truncated, infos = envs_wrapper.step(
-            actions.cpu().numpy()
-        )
-
-        ep_value_preds[step] = torch.squeeze(state_value_preds)
-        ep_rewards[step] = torch.tensor(rewards, device=device)
-        ep_action_log_probs[step] = action_log_probs
-
-        # add a mask (for the return calculation later);
-        # for each env the mask is 1 if the episode is ongoing and 0 if it is terminated (not by truncation!)
-        masks[step] = torch.tensor([not term for term in terminated])
-
-    # calculate the losses for actor and critic
-    critic_loss, actor_loss = agent.get_losses(
-        ep_rewards,
-        ep_action_log_probs,
-        ep_value_preds,
-        entropy,
-        masks,
-        gamma,
-        lam,
-        ent_coef,
-        device,
-    )
-
-    # update the actor and critic networks
-    agent.update_parameters(critic_loss, actor_loss)
-
-    # log the losses and entropy
-    critic_losses.append(critic_loss.detach().cpu().numpy())
-    actor_losses.append(actor_loss.detach().cpu().numpy())
-    entropies.append(entropy.detach().mean().cpu().numpy())
-
-
-
-""" plot the results """
-
-# %matplotlib inline
-
-rolling_length = 20
-fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 5))
-fig.suptitle(
-    f"Training plots for {agent.__class__.__name__} in the LunarLander-v2 environment \n \
-             (n_envs={n_envs}, n_steps_per_update={n_steps_per_update}, randomize_domain={randomize_domain})"
-)
-
-# episode return
-axs[0][0].set_title("Episode Returns")
-episode_returns_moving_average = (
-    np.convolve(
-        np.array(envs_wrapper.return_queue).flatten(),
-        np.ones(rolling_length),
-        mode="valid",
-    )
-    / rolling_length
-)
-axs[0][0].plot(
-    np.arange(len(episode_returns_moving_average)) / n_envs,
-    episode_returns_moving_average,
-)
-axs[0][0].set_xlabel("Number of episodes")
-
-# entropy
-axs[1][0].set_title("Entropy")
-entropy_moving_average = (
-    np.convolve(np.array(entropies), np.ones(rolling_length), mode="valid")
-    / rolling_length
-)
-axs[1][0].plot(entropy_moving_average)
-axs[1][0].set_xlabel("Number of updates")
-
-
-# critic loss
-axs[0][1].set_title("Critic Loss")
-critic_losses_moving_average = (
-    np.convolve(
-        np.array(critic_losses).flatten(), np.ones(rolling_length), mode="valid"
-    )
-    / rolling_length
-)
-axs[0][1].plot(critic_losses_moving_average)
-axs[0][1].set_xlabel("Number of updates")
-
-
-# actor loss
-axs[1][1].set_title("Actor Loss")
-actor_losses_moving_average = (
-    np.convolve(np.array(actor_losses).flatten(), np.ones(rolling_length), mode="valid")
-    / rolling_length
-)
-axs[1][1].plot(actor_losses_moving_average)
-axs[1][1].set_xlabel("Number of updates")
-
-plt.tight_layout()
-plt.show()
-
-save_weights = True
 load_weights = True
+actor_weights_path = "lunar_lander/weights/actor_weights.h5"
+critic_weights_path = "lunar_lander/weights/critic_weights.h5"
 
-actor_weights_path = "weights/actor_weights.h5"
-critic_weights_path = "weights/critic_weights.h5"
+if not(load_weights):
+    # use tqdm to get a progress bar for training
+    for sample_phase in tqdm(range(n_updates)):
+        # we don't have to reset the envs, they just continue playing
+        # until the episode is over and then reset automatically
+        
+        # reset lists that collect experiences of an episode (sample phase)
+        ep_value_preds = torch.zeros(n_steps_per_update, n_envs, device=device)
+        ep_rewards = torch.zeros(n_steps_per_update, n_envs, device=device)
+        ep_action_log_probs = torch.zeros(n_steps_per_update, n_envs, device=device)
+        masks = torch.zeros(n_steps_per_update, n_envs, device=device)
 
-if not os.path.exists("weights"):
-    os.mkdir("weights")
+        # at the start of training reset all envs to get an initial state
+        if sample_phase == 0:
+            states, info = envs_wrapper.reset(seed=42)
 
-""" save network weights """
-if save_weights:
-    torch.save(agent.actor.state_dict(), actor_weights_path)
-    torch.save(agent.critic.state_dict(), critic_weights_path)
+        # play n steps in our parallel environments to collect data
+        for step in range(n_steps_per_update):
+            # select an action A_{t} using S_{t} as input for the agent
+            actions, action_log_probs, state_value_preds, entropy = agent.select_action(
+                states
+            )
+
+            # perform the action A_{t} in the environment to get S_{t+1} and R_{t+1}
+            states, rewards, terminated, truncated, infos = envs_wrapper.step(
+                actions.cpu().numpy()
+            )
+
+            ep_value_preds[step] = torch.squeeze(state_value_preds)
+            ep_rewards[step] = torch.tensor(rewards, device=device)
+            ep_action_log_probs[step] = action_log_probs
+
+            # add a mask (for the return calculation later);
+            # for each env the mask is 1 if the episode is ongoing and 0 if it is terminated (not by truncation!)
+            masks[step] = torch.tensor([not term for term in terminated])
+
+        # calculate the losses for actor and critic
+        critic_loss, actor_loss = agent.get_losses(
+            ep_rewards,
+            ep_action_log_probs,
+            ep_value_preds,
+            entropy,
+            masks,
+            gamma,
+            lam,
+            ent_coef,
+            device,
+        )
+
+        # update the actor and critic networks
+        agent.update_parameters(critic_loss, actor_loss)
+
+        # log the losses and entropy
+        critic_losses.append(critic_loss.detach().cpu().numpy())
+        actor_losses.append(actor_loss.detach().cpu().numpy())
+        entropies.append(entropy.detach().mean().cpu().numpy())
+
+
+
+    """ plot the results """
+
+    # %matplotlib inline
+
+    rolling_length = 20
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 5))
+    fig.suptitle(
+        f"Training plots for {agent.__class__.__name__} in the LunarLander-v2 environment \n \
+                (n_envs={n_envs}, n_steps_per_update={n_steps_per_update}, randomize_domain={randomize_domain})"
+    )
+
+    # episode return
+    axs[0][0].set_title("Episode Returns")
+    episode_returns_moving_average = (
+        np.convolve(
+            np.array(envs_wrapper.return_queue).flatten(),
+            np.ones(rolling_length),
+            mode="valid",
+        )
+        / rolling_length
+    )
+    axs[0][0].plot(
+        np.arange(len(episode_returns_moving_average)) / n_envs,
+        episode_returns_moving_average,
+    )
+    axs[0][0].set_xlabel("Number of episodes")
+
+    # entropy
+    axs[1][0].set_title("Entropy")
+    entropy_moving_average = (
+        np.convolve(np.array(entropies), np.ones(rolling_length), mode="valid")
+        / rolling_length
+    )
+    axs[1][0].plot(entropy_moving_average)
+    axs[1][0].set_xlabel("Number of updates")
+
+
+    # critic loss
+    axs[0][1].set_title("Critic Loss")
+    critic_losses_moving_average = (
+        np.convolve(
+            np.array(critic_losses).flatten(), np.ones(rolling_length), mode="valid"
+        )
+        / rolling_length
+    )
+    axs[0][1].plot(critic_losses_moving_average)
+    axs[0][1].set_xlabel("Number of updates")
+
+
+    # actor loss
+    axs[1][1].set_title("Actor Loss")
+    actor_losses_moving_average = (
+        np.convolve(np.array(actor_losses).flatten(), np.ones(rolling_length), mode="valid")
+        / rolling_length
+    )
+    axs[1][1].plot(actor_losses_moving_average)
+    axs[1][1].set_xlabel("Number of updates")
+
+    plt.tight_layout()
+    plt.show()
+
+    save_weights = False
+
+    if not os.path.exists("weights"):
+        os.mkdir("weights")
+
+    """ save network weights """
+    if save_weights:
+        torch.save(agent.actor.state_dict(), actor_weights_path)
+        torch.save(agent.critic.state_dict(), critic_weights_path)
 
 
 """ load network weights """
 if load_weights:
-    agent = A2C(obs_shape, action_shape, device, critic_lr, actor_lr)
+    agent = A2C(obs_shape, action_shape, device, critic_lr, actor_lr, n_envs=1)
 
     agent.actor.load_state_dict(torch.load(actor_weights_path))
     agent.critic.load_state_dict(torch.load(critic_weights_path))
